8046a89992c0830b0185ff3fc8c6ba11eebac877
Mahdi Jalali <mahdi.jalali9087@gmail.com>
2026-06-05 15:07:53 +0330

    first commit

---
+++ Scheduler/BizAgi_FridayProject_Scheduler_Service_Install.bat
@@ -0,0 +1,3 @@
+"C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /name=BizAgiFridayProjectSchedulerService BizAgi.Scheduler.Services.exe
+--NET START "BizAgiFridayProjectSchedulerService"
+rem pause
---
+++ Scheduler/BizAgi_FridayProject_Scheduler_Service_UnInstall.bat
@@ -0,0 +1,3 @@
+"C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /u /name=BizAgiFridayProjectSchedulerService BizAgi.Scheduler.Services.exe
+sc delete BizAgiFridayProjectSchedulerService
+rem pause
